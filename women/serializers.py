b43f28a968fb45211f19f4549666978ddee13c77
Omarbek Nurkut <omarbeknurkut6@gmail.com>
2023-05-30 11:57:25 +0600

    Model Serializer

--- women/serializers.py
+++ women/serializers.py
@@ -14,25 +14,9 @@ import io
 
 
 class WomenSerializer(Serializer):
-	title = serializers.CharField(max_length=200)
-	content = serializers.CharField()
-	time_create = serializers.DateTimeField(read_only=True)
-	time_update = serializers.DateTimeField(read_only=True)
-	is_published = serializers.BooleanField(default=True)
-	cat_id = serializers.IntegerField()
-
-	def create(self, validated_data):
-		return Women.objects.create(**validated_data)
-
-
-	def update(self, instance, validated_data):
-		instance.title = validated_data.get("title", instance.title)
-		instance.content = validated_data.get("content", instance.content)
-		instance.time_update = validated_data.get("time_update", instance.time_update)
-		instance.is_published = validated_data.get("is_published", instance.is_published)
-		cat_id = validated_data.get("cat_id", instance.cat_id)
-		instance.save()
-		return instance
+	class Meta:
+		model = Women
+		fields = ("title", "content", "cat")
 
 
 
